b259d6637e917cc96ead8ed9c6526853bff572c2
leodiegues <leodieggs@gmail.com>
2024-04-18 19:28:05 -0300

    fix: installer

--- bin/dot-bootstrap.sh
+++ bin/dot-bootstrap.sh
@@ -5,15 +5,15 @@ set -e
 tags="$1"
 
 if [ -z $tags ]; then
-  tags="all"
+	tags="all"
 fi
 
 if ! [ -x "$(command -v ansible)" ]; then
-  sudo apt install ansible
+	sudo apt install ansible
 fi
 
-ansible-playbook ~/dotfiles/playbook.yml --ask-become-pass --ask-vault-pass --tags $tags
+ansible-playbook ~/.dotfiles/playbook.yml --ask-become-pass --tags $tags
 
 if command -v terminal-notifier 1>/dev/null 2>&1; then
-  terminal-notifier -title "dotfiles: Bootstrap complete" -message "Successfully set up dev environment."
+	terminal-notifier -title "dotfiles: Bootstrap complete" -message "Successfully set up dev environment."
 fi
