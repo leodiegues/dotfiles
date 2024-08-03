f24f6a91c21730dc689d6bb93a8a34b538d58016
leodiegues <leodieggs@gmail.com>
2024-08-03 12:54:57 -0300

    chore: update nvim configs

--- roles/neovim/files/lua/plugins/colorscheme.lua
+++ roles/neovim/files/lua/plugins/colorscheme.lua
@@ -1,33 +1,27 @@
-function ColorMyPencils(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        disable_background = true,
-        styles = {
-          transparent = true,
-          italic = false,
-        },
-      })
-
-      vim.cmd("colorscheme rose-pine")
-
-      ColorMyPencils()
-    end,
+    opts = {
+      disable_background = true,
+      styles = {
+        transparent = true,
+        italic = false,
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "catppuccin",
     },
   },
 }
